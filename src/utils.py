83ea6bc6fd1f3e5aa521cfe5ad6005085e704546
rioter1 <prateek.gsharma@gmail.com>
2023-04-03 17:08:00 +0530

    deployment configration

--- src/utils.py
+++ src/utils.py
@@ -31,7 +31,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
         for i in range(len(list(models))):
             model = list(models.values())[i]
             para=param[list(models.keys())[i]]
-
+            # grid search cross validation
             gs = GridSearchCV(model,para,cv=3)
             gs.fit(X_train,y_train)
 
@@ -51,6 +51,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
